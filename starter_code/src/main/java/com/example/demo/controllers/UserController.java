f29784990af044147ab3009d3fb85db3f8e2759b
Random Facades <random.facades@gmail.com>
2024-11-22 15:24:40 -0500

    feat: Configured logging to file to capture in Splunk

--- starter_code/src/main/java/com/example/demo/controllers/UserController.java
+++ starter_code/src/main/java/com/example/demo/controllers/UserController.java
@@ -5,6 +5,9 @@ import com.example.demo.model.persistence.User;
 import com.example.demo.model.persistence.repositories.CartRepository;
 import com.example.demo.model.persistence.repositories.UserRepository;
 import com.example.demo.model.requests.CreateUserRequest;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,10 +16,12 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
-	
+
+	Logger log = LogManager.getLogger(UserController.class);
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private CartRepository cartRepository;
 
@@ -27,29 +32,33 @@ public class UserController {
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		return ResponseEntity.of(userRepository.findById(id));
 	}
-	
+
 	@GetMapping("/{username}")
 	public ResponseEntity<User> findByUserName(@PathVariable String username) {
 		User user = userRepository.findByUsername(username);
 		return user == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(user);
 	}
-	
+
 	@PostMapping("/create")
 	public ResponseEntity<User> createUser(@RequestBody CreateUserRequest createUserRequest) {
+		log.info("Create User called");
 		User user = new User();
 		user.setUsername(createUserRequest.getUsername());
 		Cart cart = new Cart();
 		cartRepository.save(cart);
 		user.setCart(cart);
-		if(createUserRequest.getPassword().length()<7 ||
-				!createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword())){
-			//System.out.println("Error - Either length is less than 7 or pass and conf pass do not match. Unable to create ",
-			//		createUserRequest.getUsername());
+		if (createUserRequest.getPassword().length() < 7) {
+			log.info("Create User Failed - Password too short");
+			return ResponseEntity.badRequest().build();
+		}
+		if (!createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword())) {
+			log.info("Create User Failed - Passwords do not match");
 			return ResponseEntity.badRequest().build();
 		}
 		user.setPassword(bCryptPasswordEncoder.encode(createUserRequest.getPassword()));
 		userRepository.save(user);
+		log.info("Create User successful");
 		return ResponseEntity.ok(user);
 	}
-	
+
 }
--- starter_code/src/test/java/com/example/demo/SareetaApplicationTests.java
+++ starter_code/src/test/java/com/example/demo/SareetaApplicationTests.java
@@ -1,16 +1,119 @@
 package com.example.demo;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.net.URI;
+
+import org.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.example.demo.TestUtils.LoginRequest;
+import com.example.demo.model.requests.CreateUserRequest;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 public class SareetaApplicationTests {
 
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception {
+		assertNotNull(restTemplate);
 	}
 
+	@Test
+	public void test_create_user() throws Exception {
+		String username = "CREATE_USER";
+		String password = "CREATE_PASSWORD";
+
+		final String baseUrl = "http://localhost:8080/api/user/create";
+		URI uri = new URI(baseUrl);
+
+		CreateUserRequest req = new CreateUserRequest();
+		req.setUsername(username);
+		req.setPassword(password);
+		req.setConfirmPassword(password);
+
+		ResponseEntity<String> result = this.restTemplate.postForEntity(uri, req, String.class);
+
+		assertEquals(HttpStatus.OK, result.getStatusCode());
+		assertNotNull(result.getBody());
+	}
+
+	@Test 
+	public void test_unauthorized_access() throws Exception {
+		String username = "BAD_USER";
+		String password = "BAD_PASSWORD";
+
+		final String baseUrl = "http://localhost:8080/login";
+		URI uri = new URI(baseUrl);
+
+		LoginRequest req = new LoginRequest();
+		req.setUsername(username);
+		req.setPassword(password);
+
+		ResponseEntity<String> result = this.restTemplate.postForEntity(uri, req, String.class);
+
+		assertEquals(HttpStatus.FORBIDDEN, result.getStatusCode());
+	}
+
+
+	@Test 
+	public void test_create_login_and_check_user() throws Exception {
+		String username = "LOGIN_USER";
+		String password = "LOGIN_PASSWORD";
+
+		String baseUrl = "http://localhost:8080/api/user/create";
+		URI uri = new URI(baseUrl);
+
+		CreateUserRequest create = new CreateUserRequest();
+		create.setUsername(username);
+		create.setPassword(password);
+		create.setConfirmPassword(password);
+
+		ResponseEntity<String> result = this.restTemplate.postForEntity(uri, create, String.class);
+
+		assertEquals(HttpStatus.OK, result.getStatusCode());
+		assertNotNull(result.getBody());
+
+		baseUrl = "http://localhost:8080/login";
+		uri = new URI(baseUrl);
+
+		LoginRequest login = new LoginRequest();
+		login.setUsername(username);
+		login.setPassword(password);
+
+		result = this.restTemplate.postForEntity(uri, login, String.class);
+
+		assertEquals(HttpStatus.OK, result.getStatusCode());
+		assertNotNull(result.getHeaders().get("Authorization"));
+
+		baseUrl = "http://localhost:8080/api/user/" + username;
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", result.getHeaders().get("Authorization").get(0));
+		result = restTemplate.exchange(baseUrl, HttpMethod.GET, new HttpEntity<>(headers), String.class);
+
+		assertEquals(HttpStatus.OK, result.getStatusCode());
+		assertNotNull(result.getBody());
+
+		JSONObject json = new JSONObject(result.getBody());
+
+		assertEquals(username, json.getString("username"));
+
+	}
 }
